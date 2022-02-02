e4ea85a74f9169d625d0ccb1e349e9e8070b798c
Andres Agudelo <echeverry1129@gmail.com>
2022-02-02 09:52:03 -0500

    agregamos el index js con un objeto de usuarios

---
+++ index.js
@@ -0,0 +1,5 @@
+const users = [
+  { name: "juuan", email: "juan@mail.com", hobby: "games" },
+  { name: "pedro", email: "pedro@mail.com", hobby: "react" },
+  { name: "julio", email: "julio@mail.com", hobby: "music" },
+];
