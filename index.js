52203392c778212d6f70b1e49adca7ea4a89ab1a
andres <asagudelo30@misena.edu.co>
2022-03-22 19:37:36 -0500

    probando ssh

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const users = [
   { name: "juuan", email: "juan@mail.com", hobby: "games" },
   { name: "pedro", email: "pedro@mail.com", hobby: "react" },
   { name: "julio", email: "julio@mail.com", hobby: "music" },
+  { name: "tulio", email: "tulio@mail.com", hobby: "chamullar" },
 ];
 let cont = 1;
 document.getElementById("users").innerHTML = users.map((val) => ` Usuario ${cont++} : ` + val.name + " Correo: " + val.email + "  Hobby: " + val.hobby);
